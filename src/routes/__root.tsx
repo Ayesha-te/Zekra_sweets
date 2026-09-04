d5dcf0c9d2ed3aaeb7e1d91a60ef4435afd9ad7d
Ayesha Jahangir <ayeshajahangirrr@gmail.com>
2026-09-04 17:54:25 +0500

    Update __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -121,6 +121,23 @@ gtag('config', 'G-HHN1SZZVXK');`,
         {children}
         <Scripts />
         <script src="https://cdn.zanderio.ai/widget/loader.js" data-id="wdg_rUfeKg5Bpwfom3kVBtNxtJmh" defer></script>
+        <!-- Meta Pixel Code -->
+<script>
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '1607018814407757');
+fbq('track', 'PageView');
+</script>
+<noscript><img height="1" width="1" style="display:none"
+src="https://www.facebook.com/tr?id=1607018814407757&ev=PageView&noscript=1"
+/></noscript>
+<!-- End Meta Pixel Code -->
       </body>
     </html>
   );
